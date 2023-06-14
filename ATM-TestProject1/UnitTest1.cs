5a9d831130c2e104eeaf019f4a963ff676fd1977
AlHareth alhyari <hdhareth@gmail.com>
2023-06-14 23:19:50 +0300

    lab02-ATM

--- ATM-TestProject1/UnitTest1.cs
+++ ATM-TestProject1/UnitTest1.cs
@@ -1,7 +1,84 @@
+using ATM;
+
 namespace ATM_TestProject1
 {
     public class UnitTest1
     {
-       
+        [Fact]
+        public void ViewingBalance()
+        {
+            //Arrange
+            Program.Balance = 100;
+            //Act
+            decimal actual = Program.ViewBalance();
+            //Asserte
+            Assert.Equal(100, actual);
+        }
+        [Fact]
+        public void TestingBalanceIfItsLessThanZero()
+        {
+            //Arrange
+            Program.Balance = -10;
+            //Act
+            decimal actual = Program.ViewBalance();
+            //Asserte
+            Assert.Equal(0, actual);
+
+        }
+        [Fact]
+        public void TestingBalanceIfItsZero()
+        {
+            //Arrange
+            Program.Balance = 0;
+            //Act
+            decimal actual = Program.ViewBalance();
+            //Asserte
+            Assert.Equal(0, actual);
+
+        }
+        [Fact]
+        public void TestingDrawIfItsLessThanZero() 
+        {
+            //Arrange
+            Program.Balance = 100;
+            decimal draw = -50;
+            //Act
+            decimal actual = Program.WithDraw(draw);
+            //Asserte
+            Assert.Equal(50, actual);
+        }
+        [Fact]
+        public void TestingDrawIfItsMoreThanBalance()
+        {
+            //Arrange
+            Program.Balance = 100;
+            decimal draw = 150;
+            //Act
+            decimal actual = Program.WithDraw(draw);
+            //Asserte
+            Assert.Equal(0, actual);
+        }
+        [Fact]
+        public void TestingBalanceIfItsMoreThanOrEqualDraw()
+        {
+            //Arrange
+            Program.Balance = 100;
+            decimal draw = 90;
+            //Act
+            decimal actual = Program.WithDraw(draw);
+            //Asserte
+            Assert.Equal(10, actual);
+        }
+        [Fact]
+        public void TestingDeposit()
+        {
+            //Arrange
+            Program.Balance = 100;
+            decimal deposit = 90;
+            //Act
+            decimal actual = Program.Deposit(deposit);
+            //Asserte
+            Assert.Equal(190, actual);
+        }
     }
 }
